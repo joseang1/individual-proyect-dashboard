158aabd6ef278bcc08c5aa99a9f397804b6846dd
Jose Ángel Peña Díaz <penadiazjoseangel@gmail.com>
2026-06-05 09:27:38 +0200

    feat: Validation form
    Validation form works as intended.

--- src/scripts/password-validation.js
+++ src/scripts/password-validation.js
@@ -1,19 +1,38 @@
-const myInput = document.getElementsByClassName('password-login');
-const logValidation = document.getElementsByTagName('pre')
+const loginForm = document.forms['loginform'];
+const emailInput = document.getElementById('email-login');
+const passwordInput = document.getElementById('password-login');
+const emailLog = document.getElementById('log-email');
+const passwordLog = document.getElementById('log-psw');
 
-myInput.onkeyup = function() {
-    let lowerCaseLetters = /[a-z]/g;
-    let numberInside = /[0-9]/g;
+function validateEmail(email) {
+    return email === 'giaco@kinetic-admin.com';
+}
 
-    if (myInput.value.match(lowerCaseLetters)) {
-        logValidation.textContent = `Valid`
-    } else {
-        logValidation.textContent = `Invalid`
+function validatePassword(password) {
+    return password === 'admin12kinetic';
+}
+
+function validation() {
+    let isValid = true;
+
+    emailLog.textContent = '';
+    passwordLog.textContent = '';
+
+    if (!emailInput.value.trim()) {
+        emailLog.textContent = 'El campo no puede estar vacío';
+        isValid = false;
+    } else if (!validateEmail(emailInput.value)) {
+        emailLog.textContent = 'El correo no es correcto';
+        isValid = false;
     }
 
-    if (myInput.value.match(numberInside)) {
-        logValidation.textContent = `Valid`
-    } else {
-        logValidation.textContent = `Invalid`   
+    if (!passwordInput.value.trim()) {
+        passwordLog.textContent = 'La contraseña es requerida';
+        isValid = false;
+    } else if (!validatePassword(passwordInput.value)) {
+        passwordLog.textContent = 'La contraseña no es correcta';
+        isValid = false;
     }
+
+    return isValid;
 }
