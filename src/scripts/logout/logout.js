045d8b4754e622be6767a6413f67f5ce237bee52
Jose Ángel Peña Díaz <penadiazjoseangel@gmail.com>
2026-06-08 09:13:50 +0200

    feat: Logout button
    Logout button now replaces the employees.html with index.html

--- src/scripts/logout/logout.js
+++ src/scripts/logout/logout.js
@@ -1,3 +1,3 @@
  document.getElementById('logout-btn').addEventListener('click', function() {
-            window.location.href = '../index.html';
+            window.location.replace('../index.html');
         });
